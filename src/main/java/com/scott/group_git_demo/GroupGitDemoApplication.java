0c09e89f09ff84645ebb1e5593af7093fe0181fd
Scott Bromander <scott@primeacademy.io>
2018-04-03 11:27:17 -0700

    Initial Commit

---
+++ src/main/java/com/scott/group_git_demo/GroupGitDemoApplication.java
@@ -0,0 +1,12 @@
+package com.scott.group_git_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupGitDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/scott/group_git_demo/GroupGitDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scott.group_git_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroupGitDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
